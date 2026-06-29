debf709c917f40fcc4d3fefda81f19ed08821eb0
RAM MANOHAR REDDY <malirammanoharreddy@gmail.com>
2026-06-29 16:52:07 +0530

    Time: 0 ms (100%), Space: 56.4 MB (35.1%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack = [];
+    
+    for (let ch of s) {
+        if (ch === '(') stack.push(')');
+        else if (ch === '{') stack.push('}');
+        else if (ch === '[') stack.push(']');
+        else if (stack.pop() !== ch) return false;
+    }
+    
+    return stack.length === 0;
+};
