0530634f79aae0d3b1529b22ce21628be59a3b8c
RAM MANOHAR REDDY <malirammanoharreddy@gmail.com>
2026-07-20 15:28:23 +0530

    Time: 2 ms (31.19%), Space: 62.4 MB (15.97%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.js
@@ -0,0 +1,13 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let left = 0
+    let right = s.length-1
+    while(left<right){
+        [s[right],s[left]]=[s[left],s[right]]
+        left ++
+        right --
+    }
+};
