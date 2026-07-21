822248c8fa6cb85170a1a903e851b5efc67a1bf3
RAM MANOHAR REDDY <malirammanoharreddy@gmail.com>
2026-07-21 14:48:04 +0530

    Time: 3 ms (66.4%), Space: 60.2 MB (26.64%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    const arr = [...nums1,...nums2]
+    arr.sort((a, b) => a - b);
+
+    let mid = Math.floor(arr.length / 2);
+
+    if (arr.length % 2 === 0) {
+        return (arr[mid] + arr[mid - 1]) / 2;
+    }
+
+    return arr[mid];
+};
