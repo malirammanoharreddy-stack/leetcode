493b59b3ad7cac4edc870f7fae6dbc1c40f1aeab
RAM MANOHAR REDDY <malirammanoharreddy@gmail.com>
2026-07-21 14:37:15 +0530

    Time: 0 ms (100%), Space: 53.9 MB (91.04%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+
+    // First occurrence
+    let left = 0;
+    let right = nums.length - 1;
+    let first = -1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            first = mid;
+            right = mid - 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    // Reset pointers
+    left = 0;
+    right = nums.length - 1;
+
+    // Last occurrence
+    let last = -1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            last = mid;
+            left = mid + 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return [first, last];
+};
