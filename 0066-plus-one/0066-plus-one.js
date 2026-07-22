8d5a5999532b397f1a6439933bba4187d6ae0a07
RAM MANOHAR REDDY <malirammanoharreddy@gmail.com>
2026-07-22 16:19:50 +0530

    Time: 0 ms (100%), Space: 53.4 MB (67.44%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+let num = BigInt(digits.join('')) + 1n;
+ return num.toString().split('').map(Number);
+};
