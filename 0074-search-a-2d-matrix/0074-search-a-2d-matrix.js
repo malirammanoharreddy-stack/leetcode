2647e241bea2f609520e5e021bd9181e2d1f6644
RAM MANOHAR REDDY <malirammanoharreddy@gmail.com>
2026-07-30 00:00:33 +0530

    Time: 0 ms (100%), Space: 53.9 MB (56.4%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+   let rows = matrix.length;
+    let cols = matrix[0].length;
+
+    let left = 0;
+    let right = rows * cols - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        let row = Math.floor(mid / cols);
+        let col = mid % cols;
+
+        if (matrix[row][col] === target) {
+            return true;
+        } else if (matrix[row][col] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return false;
+}
